f7f87529526d31027421ddf1e756905dc8e4c4a7
Kutluguzhin <kutluguzhin@list.ru>
2022-06-22 16:06:59 +0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,12 @@
+TOKEN = "5570772411:AAGdptPWHk41I9hKdWENmyynjbiTI3Bu-58"
+
+keys = {
+    "доллар": "USD",
+    "евро": "EUR",
+    "эфириум": "ETH",
+    "рубль" : "RUB",
+    "биткоин" : "BTC",
+    "лира" : "TRY",
+    "юань" : "CNY",
+    "йена" : "JPY"
+}
